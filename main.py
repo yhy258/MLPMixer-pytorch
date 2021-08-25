a832d3a3c69ff2d62509cc67a473a5c08f6895e0
Seow <yhy258@naver.com>
2021-08-25 23:36:04 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,63 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+
+from module import MLPMixer
+
+from tqdm.notebook import tqdm
+import numpy as np
+
+dataset = datasets.CIFAR10(
+    root = "./.data",
+    train = True,
+    transform =transforms.Compose([transforms.ToTensor(), transforms.Normalize(0.5, 0.5)]),
+    download = True
+)
+
+dataset, valid_dataset = torch.utils.data.random_split(dataset, [45000, 5000])
+
+dataloader = DataLoader(dataset, batch_size =32, shuffle = True)
+val_dataloader = DataLoader(valid_dataset, batch_size =32, shuffle = True)
+
+DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model = MLPMixer(3, 256, 128, 1024, img_size = 32, patch_size = 4, depth = 8, num_classes = 10)
+model = model.to(DEVICE)
+
+optim = torch.optim.Adam(params=model.parameters(), lr=1e-4)
+criterion = nn.CrossEntropyLoss()
+
+for epoch in range(50):
+    losses = []
+    model.train()
+    for img, label in tqdm(dataloader):
+        img = img.to(DEVICE)
+        label = label.to(DEVICE)
+
+        pred = model(img)
+
+        loss = criterion(pred, label)
+        losses.append(loss)
+
+        optim.zero_grad()
+        loss.backward()
+        optim.step()
+
+    model.eval()
+    scores = 0
+    all_data = 0
+    with torch.no_grad():
+        for val_image, val_label in tqdm(val_dataloader):
+            val_image = val_image.to(DEVICE)
+            val_label = val_label.to(DEVICE)
+
+            val_pred = model(val_image)
+            arg = F.softmax(val_pred, dim=1).argmax(dim=1)
+            score = (arg == val_label).sum().item()
+            scores += score
+            all_data += len(val_image)
+
+    print("Loss : {} Score : {}".format(np.mean(losses), scores / all_data))
+
---
+++ module.py
@@ -0,0 +1,88 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+
+class MLP(nn.Module):
+    def __init__(self, dim, hid_dim, dropout=0.):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(dim, hid_dim),
+            nn.GELU(),
+            nn.Dropout(dropout),
+            nn.Linear(hid_dim, dim),
+            nn.Dropout(dropout)
+        )
+
+    def forward(self, x):
+        return self.net(x)
+
+
+"""
+    Pre layernorm
+"""
+
+
+class TokenMixing(nn.Module):
+    def __init__(self, dim, patch_num, token_mix):
+        super().__init__()
+        self.layernorm = nn.LayerNorm(dim)
+
+        self.tokenmix = MLP(patch_num, token_mix)
+
+    def forward(self, x):
+        x = self.layernorm(x)  # bs, n, c -> bs, c ,n
+        x = x.transpose(1, 2)
+        x = self.tokenmix(x)
+        x = x.transpose(1, 2)  # bs,n , c
+        return x
+
+
+class ChannelMixing(nn.Module):
+    def __init__(self, dim, channel_mix):
+        super().__init__()
+        self.layernorm = nn.LayerNorm(dim)
+        self.channelmix = MLP(dim, channel_mix)
+
+    def forward(self, x):
+        x = self.layernorm(x)
+        x = self.channelmix(x)
+        return x
+
+
+class MLPMixer(nn.Module):
+    def __init__(self, in_channels, dim, token_mix, channel_mix, img_size=32, patch_size=4, depth=8, num_classes=10):
+        super().__init__()
+        assert img_size % patch_size == 0, 'Image dimensions must be divisible by the patch size.'
+
+        self.num_patch = (img_size // patch_size) ** 2
+
+        self.dim = dim
+        self.conv = nn.Conv2d(in_channels, dim, patch_size, patch_size)
+        self.module_list = nn.ModuleList([])
+
+        for _ in range(depth):
+            self.module_list.append(TokenMixing(dim, self.num_patch, token_mix))
+            self.module_list.append(ChannelMixing(dim, channel_mix))
+
+        self.layernorm = nn.LayerNorm(dim)
+
+        self.mlp_head = nn.Sequential(
+            nn.Linear(dim, num_classes)
+        )
+
+    def forward(self, x):
+        bs = x.size(0)
+        x = self.conv(x)  # bs, c, h, w
+
+        x = x.view(bs, self.dim, -1)
+        x = x.transpose(1, 2)  # bs, (h, w), c
+
+        for mixer_block in self.module_list:
+            x = mixer_block(x)
+
+        x = self.layernorm(x)
+        x = x.mean(dim=1)
+
+        return self.mlp_head(x)
